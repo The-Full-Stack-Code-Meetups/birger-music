02358628d37d1b1249fc1b9feed0815fbeba6e2d
cosmohacker <yagizcanyavuz@outlook.com>
2024-03-08 23:29:14 +0200

    First Version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# birger-music
+# Music for Birger
+
+<img width="250px" src="birger.jpg"/>
+
+This repository is created for Birger Discord Bot.
+
+## Instructions :
+
+config.py
+Change this lines with your bot and spotify credentials:
+BOT_TOKEN: str = ""
+SPOTIFY_ID: str = ""
+SPOTIFY_SECRET: str = ""
---
+++ musicbot/commands/general.py
@@ -0,0 +1,113 @@
+import discord
+from config import config
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+from musicbot import utils
+from musicbot.audiocontroller import AudioController
+from musicbot.utils import guild_to_audiocontroller, guild_to_settings
+
+
+class General(commands.Cog):
+    """ A collection of the commands for moving the bot around in you server.
+
+            Attributes:
+                bot: The instance of the bot that is executing the commands.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # logic is split to uconnect() for wide usage
+    @commands.command(name='connect', description=config.HELP_CONNECT_LONG, help=config.HELP_CONNECT_SHORT, aliases=['c'])
+    async def _connect(self, ctx):  # dest_channel_name: str
+        current_guild = utils.get_guild(self.bot, ctx.message)
+        audiocontroller = utils.guild_to_audiocontroller[current_guild]
+        await audiocontroller.uconnect(ctx)
+
+    @commands.command(name='disconnect', description=config.HELP_DISCONNECT_LONG, help=config.HELP_DISCONNECT_SHORT, aliases=['dc'])
+    async def _disconnect(self, ctx, guild=False):
+        current_guild = utils.get_guild(self.bot, ctx.message)
+        audiocontroller = utils.guild_to_audiocontroller[current_guild]
+        await audiocontroller.udisconnect()
+
+    @commands.command(name='reset', description=config.HELP_DISCONNECT_LONG, help=config.HELP_DISCONNECT_SHORT, aliases=['rs', 'restart'])
+    async def _reset(self, ctx):
+        current_guild = utils.get_guild(self.bot, ctx.message)
+
+        if current_guild is None:
+            await ctx.send(config.NO_GUILD_MESSAGE)
+            return
+        await utils.guild_to_audiocontroller[current_guild].stop_player()
+        await current_guild.voice_client.disconnect(force=True)
+
+        guild_to_audiocontroller[current_guild] = AudioController(
+            self.bot, current_guild)
+        await guild_to_audiocontroller[current_guild].register_voice_channel(ctx.author.voice.channel)
+
+        await ctx.send("{} Connected to {}".format("", ctx.author.voice.channel.name))
+
+    @commands.command(name='changechannel', description=config.HELP_CHANGECHANNEL_LONG, help=config.HELP_CHANGECHANNEL_SHORT, aliases=['cc'])
+    async def _change_channel(self, ctx):
+        current_guild = utils.get_guild(self.bot, ctx.message)
+
+        vchannel = await utils.is_connected(ctx)
+        if vchannel == ctx.author.voice.channel:
+            await ctx.send("{} Already connected to {}".format("", vchannel.name))
+            return
+
+        if current_guild is None:
+            await ctx.send(config.NO_GUILD_MESSAGE)
+            return
+        await utils.guild_to_audiocontroller[current_guild].stop_player()
+        await current_guild.voice_client.disconnect(force=True)
+
+        guild_to_audiocontroller[current_guild] = AudioController(
+            self.bot, current_guild)
+        await guild_to_audiocontroller[current_guild].register_voice_channel(ctx.author.voice.channel)
+
+        await ctx.send("{} Switched to {}".format("", ctx.author.voice.channel.name))
+
+    @commands.command(name='birger', description=config.HELP_BIRGER_LONG, help=config.HELP_BIRGER_SHORT)
+    async def _birger(self, ctx):
+        await ctx.send("My name is Birger.")
+
+    @commands.command(name='about', description=config.HELP_BIRGER_ABOUT_LONG, help=config.HELP_BIRGER_ABOUT_SHORT)
+    async def _about(self, ctx):
+        await ctx.send("Birgeeeeeeeeeeeeeeeer")
+
+    @commands.command(name='explain', description=config.HELP_BIRGER_EXPLAIN_LONG, help=config.HELP_BIRGER_EXPLAIN_SHORT)
+    async def _explain(self, ctx):
+        await ctx.send("Birgeeeeeeeeeeeeeeeer raaah!!!")
+
+    @commands.command(name='setting', description=config.HELP_SHUFFLE_LONG, help=config.HELP_SETTINGS_SHORT, aliases=['settings', 'set'])
+    @has_permissions(administrator=True)
+    async def _settings(self, ctx, *args):
+
+        sett = guild_to_settings[ctx.guild]
+
+        if len(args) == 0:
+            await ctx.send(embed=await sett.format())
+            return
+
+        args_list = list(args)
+        args_list.remove(args[0])
+
+        response = await sett.write(args[0], " ".join(args_list), ctx)
+
+        if response is None:
+            await ctx.send("`Error: Setting not found`")
+        elif response is True:
+            await ctx.send("Setting updated!")
+
+    @commands.command(name='addbot', description=config.HELP_ADDBOT_LONG, help=config.HELP_ADDBOT_SHORT)
+    async def _addbot(self, ctx):
+        embed = discord.Embed(title="Invite", description=config.ADD_MESSAGE +
+                              "(https://discordapp.com/oauth2/authorize?client_id={}&scope=bot".format(self.bot.user.id))
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(General(bot))
+
+
